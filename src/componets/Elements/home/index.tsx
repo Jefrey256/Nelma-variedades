ac7e775751081567736a2fc01fde314befcd0680
Your Name <you@example.com>
2024-08-16 21:39:17 -0300

    subindo

---
+++ src/componets/Elements/home/index.tsx
@@ -0,0 +1,16 @@
+import './home.css' 
+import ft from '../../Imgs/bone.png'
+export const Home = () => {
+    return(
+        <div>
+            <h1 className='pico'>Seja bem vindo a  Nelma Variedades</h1>
+            <div className='ter'>
+            <a href="https://cdn77-pic.xvideos-cdn.com/videos/thumbs169poster/a0/41/c0/a041c0eeebbccf8c9d6985b492d6273b/a041c0eeebbccf8c9d6985b492d6273b.11.jpg">
+            <img className='pico' src={ft} alt="chapeu" />
+            <h1>clique na img</h1>
+            </a>
+            </div>
+            <button>Click</button>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Rotas from './componets/Routers/rotas'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Router>
+    <Rotas />
+  </Router>,
 )
